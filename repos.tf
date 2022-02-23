1e046037c918c9595e5f0b8dd40ab4c3d01b7f58
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2022-02-23 21:55:08 +0000

    Added repos for writing projects

--- repos.tf
+++ repos.tf
@@ -64,22 +64,6 @@ resource "github_repository" "do-docker" {
   }
 }
 
-resource "github_repository" "writing" {
-  name                 = "writing"
-  description          = "Ideas and thoughts for writing projects"
-  has_downloads        = true
-  has_issues           = true
-  has_wiki             = true
-  has_projects         = true
-  homepage_url         = ""
-  vulnerability_alerts = false
-  visibility           = "private"
-  topics               = ["writing"]
-  lifecycle {
-    ignore_changes = [etag]
-  }
-}
-
 resource "github_repository" "structure-of-ice-giants" {
   name                 = "structure-of-ice-giants"
   description          = "An attempt to reproduce modelled results of the internal structures of Uranus and Neptune"
---
+++ writing.tf
@@ -0,0 +1,46 @@
+resource "github_repository" "writing" {
+  name                 = "writing"
+  description          = "Ideas and thoughts for writing projects"
+  has_downloads        = true
+  has_issues           = true
+  has_wiki             = true
+  has_projects         = true
+  homepage_url         = ""
+  vulnerability_alerts = false
+  visibility           = "private"
+  topics               = ["writing"]
+  lifecycle {
+    ignore_changes = [etag]
+  }
+}
+
+locals {
+  stories = [
+    "elevated",
+    "gate-four",
+    "veteran-of-the-red-soil",
+    "the-penthouse",
+    "the-phoenix-mage",
+    "skylon-tower",
+    "the-rockets-beneath-us",
+    "the-kingdom-of-tharg",
+    "another-man",
+    "iatkh",
+  ]
+}
+
+resource "github_repository" "story" {
+  count                = length(local.stories)
+  name                 = local.stories[count.index]
+  has_downloads        = true
+  has_issues           = true
+  has_wiki             = true
+  has_projects         = true
+  homepage_url         = ""
+  vulnerability_alerts = false
+  visibility           = "private"
+  topics               = ["writing"]
+  lifecycle {
+    ignore_changes = [etag]
+  }
+}
